bf01934c833f1b30fadfcee550e989789c84f0a2
Dom <111844962+dom-io@users.noreply.github.com>
2023-03-23 17:04:23 -0700

    Add project files.

---
+++ FilmLibrary V2/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmLibrary_V2.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ReleaseYear { get; set; }
+    }
+}
---
+++ FilmLibrary V2/Services/ScrapeService.cs
@@ -0,0 +1,38 @@
+﻿using HtmlAgilityPack;
+
+namespace FilmLibrary_V2.Services
+{
+    public class ScrapeService
+    {
+
+        public async Task<List<string>> StartScrape()
+        {
+            List<string> scrapeListTitles = new List<string>();
+            //List<string> scrapeListRatings = new List<string>();
+
+            HttpClient hc = new HttpClient();
+            HttpResponseMessage result = await hc.GetAsync($"https://www.imdb.com/chart/top/?ref_=nv_mv_250");
+            Stream stream = await result.Content.ReadAsStreamAsync();
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(stream);
+
+            var TitleNames = doc.DocumentNode.SelectNodes("//td[@class='titleColumn']");
+            var Ratings = doc.DocumentNode.SelectNodes("//td//td[@class='titleColumn']");
+
+            foreach (var titleName in TitleNames)
+            {
+                scrapeListTitles.Add(titleName.InnerText);
+            }
+            return scrapeListTitles;
+
+            //foreach ( var rating in Ratings)
+            //{
+            //    scrapeListRatings.Add(rating.InnerText);
+            //}
+
+            // return scrapeListRatings;
+        }
+
+       
+    }
+}
